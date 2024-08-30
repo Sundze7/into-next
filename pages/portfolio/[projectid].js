aeb845cd3ae6d4f328f0b4d51261a34b0f229c9a
Sundze <keyonmwesundze@mail.com>
2024-08-30 15:58:22 +0100

    page-based routing

---
+++ pages/portfolio/[projectid].js
@@ -0,0 +1,17 @@
+import { useRouter } from "next/router";
+import PortfolioPage from './index';
+
+function PortfolioProjectPage() {
+    const router = useRouter()
+
+    console.log(router.pathname)
+    console.log(router.query)
+
+    return (
+        <div>
+            <h1>The Portfolio Project Page</h1>
+        </div>
+    )
+}
+
+export default PortfolioProjectPage
---
+++ pages/portfolio/list.js
@@ -0,0 +1,7 @@
+function ListPage() {
+    return <div>
+        <h1>The List Page</h1>
+    </div>
+}
+
+export default ListPage;
