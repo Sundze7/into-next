d6a26c1d3288d07fd294b96df6950cf9c121b629
Sundze <keyonmwesundze@mail.com>
2024-08-31 17:38:38 +0100

    dynamic and programmatic routing

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -5,8 +5,21 @@ function ClientProjectsPage() {
 
     console.log(router.query)
 
+    function loadProjectHandler() {
+        // Load data ...
+        // router.push('/clients/max/projecta')
+
+        // OR
+        router.push({
+            pathname: '/clients/[id]/[clientprojectid]',
+            query: {id: 'max', clientprojectid: 'projecta'}
+        })
+
+    }
+
     return <div>
         <h1>The Projects of a Given Client</h1>
+        <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
 }
 
--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,29 @@
+import Link from "next/link"
+
 function ClientPage() {
+    const clients = [
+        { id: "max", name: "Maximilian" },
+        { id: "manuel", name: "Manuel" }
+    ]
+
     return (
         <div>
             <h1>The Clients Page</h1>
+            {/* <ul>
+            {clients.map((client) => 
+                <li key={client.id}>
+                    <Link href={`/clients/${client.id}`}>{client.name}</Link>
+                </li>
+            )}
+            </ul> */}
+            {/* OR */}
+            <ul>
+                {clients.map((client) => (
+                    <li key={client.id}>
+                        <Link href={{pathname: '/clients/[id]', query: {id: client.id}}}>{client.name}</Link>
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
